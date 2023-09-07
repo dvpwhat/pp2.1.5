363ea8f075dfd3604fd5ba5956a27991880ae778
alevy <a.levyakov95@gmail.com>
2023-09-07 18:48:37 +0300

    di set, final,  87

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -9,6 +9,11 @@ public class Duck5 {
     @Autowired
     private Egg6 egg;
 
+    @Autowired
+    public void setEgg(Egg6 egg) {
+        this.egg = egg;
+    }
+
     @Override
     public String toString() {
         return ", в утке яйцо " + egg.toString();
